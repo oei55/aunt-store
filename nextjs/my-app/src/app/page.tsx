b60934ed130afbd6667a047fe0fcaf3be293f3db
oei55 <chnithryut1995@gmail.com>
2024-01-01 20:56:47 +0700

    \ ignore this /

---
+++ nextjs/my-app/src/app/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Image from 'next/image'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <>
+    <div className={styles.test}> 
+    
+    <button className={styles.btn}><p>home</p></button>
+
+    
+    </div>
+    <div className={styles.dd}>.</div>
+    </>
+  )
+}
